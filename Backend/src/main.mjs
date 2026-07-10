f7d8a11c0155505aa5ab036056d9fce9796c8872
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-07-10 10:50:10 +0530

    Refactor backend structure: update package.json and package-lock.json, add user routes

--- Backend/src/main.mjs
+++ Backend/src/main.mjs
@@ -1,6 +1,26 @@
 import express from "express";
+import usersroute from "./routes/user.routes.mjs";
 
 const app = express();
-const port
+
+const port = 3000;
+
+
+// Middleware 
+app.use("/users", users);
+
+
+app.get("/", (req,res)=>{
+    res.send("Hello world ");
+
+})
+
+app.listen(port,()=>{
+
+    console.log(`Example app listening on port ${port}`);
+
+});
+
+
 
 
